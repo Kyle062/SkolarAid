23a1e715b6d8c702040a724b9158634bc3bfb5a8
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2026-04-11 02:53:35 +0800

    Add project files.

---
+++ SkolarAid/Dashboard.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkolarAid
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void sataButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void sataButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sataPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void sataPanel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
